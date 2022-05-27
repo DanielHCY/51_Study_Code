58f0af9b3d317db3957ce4fc5626a87b02bd559b
DanielHCY <104087628+DanielHCY@users.noreply.github.com>
2022-05-27 15:19:43 +0800

    Add files via upload

---
+++ 矩阵键盘/Delay.c
@@ -0,0 +1,16 @@
+#include <INTRINS.H>
+
+void Delay(unsigned int ms)		//@11.0592MHz
+{
+	unsigned char i,j,k;
+	for(k=0;k<ms;k++)
+	{
+		_nop_();
+		i = 2;
+		j = 199;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
